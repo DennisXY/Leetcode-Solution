69a382cba9e4d3e2f5dc7b9fd3354ed98c0a5203
Dennis <694167481@qq.com>
2022-03-01 12:04:30 -0500

    first commit

--- 141. 环形链表.py
+++ 141. 环形链表.py
@@ -3,35 +3,18 @@
 # To represent a cycle in the given linked list,
 # we use an integer pos which represents the position (0-indexed)
 # in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
-#
-# Example 1:
-#
-# Input: head = [3,2,0,-4], pos = 1
-# Output: true
-# Explanation: There is a cycle in the linked list, where tail connects to the second node.
-# Example 2:
-#
-# Input: head = [1,2], pos = 0
-# Output: true
-# Explanation: There is a cycle in the linked list, where tail connects to the first node.
 
-# Example 3:
-#
-# Input: head = [1], pos = -1
-# Output: false
-# Explanation: There is no cycle in the linked list.
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        if not head:
+    def hasCycle(self, head) -> bool:
+        if not head or not head.next:
             return False
-        if not head.next:
-            return False
-        fast = head
-        slow = head
-        while fast != slow:
-            if not fast or not slow:
+        slow, fast = head, head.next
+        while fast:
+            if not fast.next:
                 return False
+            if slow == fast:
+                return True
             fast = fast.next.next
             slow = slow.next
-        return True
+        return False
