c35125de4109bcce02eb54682a48b6457704d818
Dennis <694167481@qq.com>
2022-03-02 11:20:51 -0500

    push

---
+++ 148. 排序链表.py
@@ -0,0 +1,21 @@
+# Given the head of a linked list,
+# return the list after sorting it in ascending order.
+
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None
+        left, right = self.sortList(head), self.sortList(mid)
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val:
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
