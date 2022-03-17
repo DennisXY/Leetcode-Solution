9b4fe9f5686828e0eb1be67ee575485c07b1bd94
Dennis <694167481@qq.com>
2022-03-17 10:59:39 -0400

    1

--- 300. 最长上升子序列.py
+++ 300. 最长上升子序列.py
@@ -4,36 +4,62 @@
 #
 # Input: [10,9,2,5,3,7,101,18]
 # Output: 4
-# Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+# Explanation: The longest increasing subsequence is [2,3,7,101],
+# therefore the length is 4.
 # Note:
 #
-# There may be more than one LIS combination, it is only necessary for you to return the length.
+# There may be more than one LIS combination, it is only necessary
+# for you to return the length.
 # Your algorithm should run in O(n2) complexity.
 # Follow up: Could you improve it to O(n log n) time complexity?
 
 class Solution:
-    def lengthOfLIS(self, nums):
+    def lengthOfLIS(self, nums) -> int:
         if not nums:
             return 0
-        dp = []
+        dp = [1] * len(nums)
         for i in range(len(nums)):
-            dp.append(1)
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
         return max(dp)
 
+# nlog版本 贪心+二分
+# 官方题解 part 2：
+# https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
+class Solution:
+    def lengthOfLIS(self, nums) -> int:
+        d = []
+        #  d[i]，表示长度为i的最长上升子序列的末尾元素的最小值
+        # 用 len 记录目前最长上升子序列的长度，起始时len为1，d[1]=nums[0]
+
+        for n in nums:
+            if not d or n > d[-1]:
+                d.append(n)
+            else:
+                l, r = 0, len(d) - 1
+                loc = r
+                while l <= r:
+                    mid = (l + r) // 2
+                    if d[mid] >= n:
+                        loc = mid
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                d[loc] = n
+        return len(d)
+
+
 # 需要返回最小字符串版本
 class Solution:
-    def LIS(self, arr):
-        length = len(arr)
-        dp = [1] * length
-        for i in range(length - 2, -1, -1):
-            for j in range(length - 1, i, -1):
-                if arr[i] < arr[j] and dp[i] <= dp[j]:
-                    dp[i] += 1
+    def LIS(self, nums):
+        length = len(nums)
+        dp = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
         max_length = max(dp)
-        temp = max_length
         result = list()
 
         for i in range(length):
@@ -43,9 +69,9 @@ class Solution:
                     if dp[j] != dp[i]:
                         i = j - 1
                         break
-                    if arr[min_idx] > arr[j]:
-                        if not result or arr[j] > result[-1]:
+                    if nums[min_idx] > nums[j]:
+                        if not result or nums[j] > result[-1]:
                             min_idx = j
-                result.append(arr[min_idx])
+                result.append(nums[min_idx])
                 max_length -= 1
         return result
