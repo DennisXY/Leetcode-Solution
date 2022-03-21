93ef286db5ea64e274ddeb5199242725757f8786
Dennis <694167481@qq.com>
2022-03-21 12:02:20 -0400

    1

--- 394. 字符串解码.py
+++ 394. 字符串解码.py
@@ -14,20 +14,9 @@
 #
 # Input: s = "3[a]2[bc]"
 # Output: "aaabcbc"
-# Example 2:
-#
-# Input: s = "3[a2[c]]"
-# Output: "accaccacc"
-# Example 3:
-#
-# Input: s = "2[abc]3[cd]ef"
-# Output: "abcabccdcdcdef"
-# Example 4:
-#
-# Input: s = "abc3[cd]xyz"
-# Output: "abccdcdcdxyz"
+# Same question 227
 
-#Same question 227
+
 class Solution:
     def decodeString(self, s: str) -> str:
         stack, res, multi = list(), "", 0
