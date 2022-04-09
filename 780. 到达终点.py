ce0d929bf69536c6e6a1e2e91caa8dd848292196
Dennis <694167481@qq.com>
2022-04-09 12:12:11 -0400

    1

--- 780. 到达终点.py
+++ 780. 到达终点.py
@@ -1,6 +1,8 @@
-# A move consists of taking a point (x, y) and transforming it to either (x, x+y) or (x+y, y).
+# A move consists of taking a point (x, y) and transforming it to either
+# (x, x+y) or (x+y, y).
 #
-# Given a starting point (sx, sy) and a target point (tx, ty), return True if and only if a sequence of moves exists to
+# Given a starting point (sx, sy) and a target point (tx, ty),
+# return True if and only if a sequence of moves exists to
 # transform the point (sx, sy) to (tx, ty). Otherwise, return False.
 #
 # Examples:
@@ -11,16 +13,7 @@
 # (1, 1) -> (1, 2)
 # (1, 2) -> (3, 2)
 # (3, 2) -> (3, 5)
-#
-# Input: sx = 1, sy = 1, tx = 2, ty = 2
-# Output: False
-#
-# Input: sx = 1, sy = 1, tx = 1, ty = 1
-# Output: True
-#
-# Note:
-#
-# sx, sy, tx, ty will all be integers in the range [1, 10^9]
+
 
 class Solution:
     def reachingPoints(self, sx, sy, tx, ty):
@@ -40,20 +33,21 @@ class Solution:
         #
         # return dp[tx][ty]
 
-        #反复使用 {tx, ty} 中较大的值减去较小的值更新点，到达点 {sx, sy} 时返回 true。使用模运算加速求解父点的过程。
-        while tx >= sx and ty >= sy:
-            if tx == ty:
-                break
-            elif tx > ty:
-                if ty > sy:
-                    tx %= ty
-                else: # ty == sy
-                    return (tx - sx) % ty == 0
+        # 反复使用 {tx, ty} 中较大的值减去较小的值更新点，到达点 {sx, sy} 时返回 true。使用模运算加速求解父点的过程。
+        while sx < tx != ty > sy:
+            if tx > ty:
+                tx %= ty
             else:
-                if tx > sx:
-                    ty %= tx
-                else:
-                    return (ty - sy) % tx == 0
+                ty %= tx
+        if tx == sx and ty == sy:
+            return True
+        elif tx == sx:
+            return ty > sy and (ty - sy) % tx == 0
+        elif ty == sy:
+            return tx > sx and (tx - sx) % ty == 0
+        else:
+            return False
+
 
 if __name__ == '__main__':
     a, b, c, d = 10, 2, 2, 11
